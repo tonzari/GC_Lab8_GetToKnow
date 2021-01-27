731320110477ad3966122b2abd5cbaf698f99bb8
antonio <antoniomanzari@gmail.com>
2021-01-27 15:23:15 -0500

    init commit

---
+++ GC_Lab8_GetToKnow/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace GC_Lab8_GetToKnow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+/*
+Ramon Guarnes / Tigard, OR / Burgers
+Antonio Manzari / Beverly Hills, MI / Focaccia Barese
+Joshua Carolin / Novi, MI / Naleśniki
+Nick D'Oria / Canton, MI / Tacos
+Jeremiah Wyeth / Crystal, MI / Burgers
+Wendi Magee / Detroit, MI / Salami
+Juliana / Denver, CO / Tacos
+Nathaniel Davis / Berkley, MI / Steak
+Tommy Waalkes / Raleigh, NC / Chicken Curry
+Grace Symore / Mesa, AZ / Sweet Potato Fries
+Jeffrey Wohlfield / Detroit, MI / Steak
+Josh Gallentine / Baldwin, MI / Falafel
+*/
