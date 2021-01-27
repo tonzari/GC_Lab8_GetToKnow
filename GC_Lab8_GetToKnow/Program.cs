723823d1019545f4ef3d9ba6a873e0e7fb2b0f45
antonio <antoniomanzari@gmail.com>
2021-01-27 15:34:49 -0500

    added the example console text

--- GC_Lab8_GetToKnow/Program.cs
+++ GC_Lab8_GetToKnow/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GC_Lab8_GetToKnow
 {
@@ -6,7 +7,19 @@ namespace GC_Lab8_GetToKnow
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to our C# class. Which student would you like to learn more about? (enter anumber 1-20): ");
+            Console.WriteLine("That student does not exist. Please try again. (enter a number 1-20)");
+            Console.WriteLine("Student 10 is Kim Driscoll. What would you like to know about Kim? (enter “hometown” or“favorite food”): ");
+            Console.WriteLine("That data does not exist. Please try again. (enter “hometown” or “favorite food”): ");
+            Console.WriteLine("Kim is from Detroit, MI. Would you like to know more? (enter “yes” or “no”): ");
+
+            ExitApp();
+        }
+
+        public static void ExitApp()
+        {
+            Console.WriteLine("Thanks!");
+            Console.WriteLine("Exiting application...");
         }
     }
 }
